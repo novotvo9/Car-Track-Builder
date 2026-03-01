c4334827a3076d6da78f07254c50f630a0ed6e05
Vojtech Novotny <141565593+novotvo9@users.noreply.github.com>
2026-03-01 16:41:15 +0100

    Basic structure

---
+++ src/modules/grid.js
@@ -0,0 +1,4 @@
+// mřížka (20 na 20) nebo dynamické
+// klikem se kreslí trať - když kliknu víckrát mění se typ políčka
+// localstorage!!!
+// menu a editor - uživatelská rozhraní (skrýt editor a načíst mapu se seznamem tratí) .hidden
