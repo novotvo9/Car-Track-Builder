2693ddd6af1831d83abd0ddd761240139fd2b6fb
novotvo9 <novotnyvojtech@sssvt.cz>
2026-03-11 16:50:25 +0100

    Loading Map Functionality

--- src/modules/ui.js
+++ src/modules/ui.js
@@ -1,15 +1,17 @@
-import { initGrid, getMapData } from "./grid.js";
-import { saveMap } from "./storage.js";
+import { initGrid, getMapData, setMapData, renderGrid } from "./grid.js";
+import { saveMap, loadMap } from "./storage.js";
 
 export function initApp() {
 
     const newMapBtn = document.getElementById("newMapBtn");
     const backBtn = document.getElementById("backToMenuBtn");
     const saveBtn = document.getElementById("saveMapBtn");
+    const loadBtn = document.getElementById("loadMapBtn");
 
     newMapBtn.addEventListener("click", startNewMap);
     backBtn.addEventListener("click", showMenu);
     saveBtn.addEventListener("click", saveCurrentMap);
+    loadBtn.addEventListener("click", loadCurrentMap);
 }
 
 function startNewMap() {
@@ -35,4 +37,23 @@ function saveCurrentMap() {
     const mapData = getMapData();
 
     saveMap(name, mapData);
+}
+
+function loadCurrentMap() {
+
+    const name = document.getElementById("loadMapNameInput").value;
+    const loadedMap = loadMap(name);
+
+    if (loadedMap === null) {
+        return;
+    }
+
+    const menuScreenEl = document.getElementById("menuScreen");
+    const editorScreenEl = document.getElementById("editorScreen");
+
+    menuScreenEl.classList.add("hidden");
+    editorScreenEl.classList.remove("hidden");
+
+    setMapData(loadedMap);
+    renderGrid();
 }
